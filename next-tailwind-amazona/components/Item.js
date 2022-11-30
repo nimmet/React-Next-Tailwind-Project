86950f3d90f369a973836a488d49dc542abbdcbe
@nimmet <nimetullah@live.com>
2022-11-30 17:51:42 +0300

    added Item component

---
+++ next-tailwind-amazona/components/Item.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Item = ({image,name,price,brand}) => {
+  return (
+    <div className=' flex flex-col items-center rounded-md w-[300px] shadow-md'>
+        <Image src={image} width={300} height={320} alt={name} className=" rounded-t-md"/>
+
+        <div className=' flex flex-col justify-center items-center gap-y-1 my-5'>
+            <h1 className=' text-xl font-[400]'>{name}</h1>
+            <h1 className=' text-xl font-[400]'>{brand}</h1>
+            <h1 className=' text-xl font-[400]'>${price}</h1>
+            <button className=' text-xl font-[400] px-3 py-2 bg-amber-300 rounded-md hover:bg-amber-400  '>Add to cart</button>
+        </div>
+    </div>
+  )
+}
+
+export default Item
