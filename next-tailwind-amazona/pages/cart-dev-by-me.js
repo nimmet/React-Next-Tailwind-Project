8048371260cbbbd46f59843176e750940c0a38d6
@nimmet <nimetullah@live.com>
2022-12-02 11:58:09 +0300

    added cart component

---
+++ next-tailwind-amazona/pages/cart-dev-by-me.js
@@ -0,0 +1,87 @@
+import Image from "next/image";
+import React, { useContext } from "react";
+import { Layout } from "../components/Layout";
+import { Store } from "../utils/Store";
+import { TiDeleteOutline } from "react-icons/ti";
+
+const cart = () => {
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
+  let total = 0
+
+  const removeItemHandler = (slug) => {
+
+    const existingCartItems = cart.cartItems.find((cartItem)=> cartItem.slug === slug)
+
+    if(existingCartItems.quantity===1){
+        return cart.cartItems.filter(cartItem => cartItem.slug !== slug)
+    }
+
+    return cart.cartItems.map((cartItem)=> cartItem.slug === slug ? {...cartItem,quantity:cartItem.quantity-1} : cartItem
+    )
+
+  }
+  
+
+  return (
+    <Layout title="Shopping Cart">
+      <div >
+        <h1 className=" text-lg font-[600]">Shopping Cart</h1>
+
+       
+          <div className=" grid grid-cols-1 md:grid-cols-4 justify-center gap-10">
+          <div className=" md:col-span-3">
+            <div className=" border-b border-b-gray-200" >
+              <ul className="grid grid-cols-4 justify-start items-center py-4 text-lg font-bold">
+                <li className="">Item</li>
+                <li>Quantity</li>
+                <li>Price</li>
+                <li>Action</li>
+              </ul>
+            </div>
+
+            <div>
+              {cart.cartItems.map((item, index) => {
+                total += item.price * item.quantity
+                return (
+                  <div key={index} className="grid grid-cols-4 justify-start items-center gap-2 border-b border-b-gray-200 py-4">
+                  <div className=" justify-start items-center gap-2 flex">
+                    <div className=" w-10 h-10">
+                      <Image
+                        src={item.image}
+                        alt={item.name}
+                        width={25}
+                        height={25}
+                        layout="responsive"
+                      />
+                    </div>
+                    <h1 className=" ">{item.name}</h1>
+                    </div>
+                    <h1>{item.quantity}</h1>
+                    <h1>${item.price}</h1>
+                    <h1>
+                      <TiDeleteOutline className=" cursor-pointer" size={30}
+                       onClick= {()=>removeItemHandler(item.slug)}/>
+                    </h1>
+                  </div>
+                );
+              })}
+            </div>
+</div>
+            <div className="card h-min">
+              <div className="mx-5 flex justify-between items-center mt-3 text-lg font-bold">
+                <h1 className=" ">Subtotal ({cart.cartItems.length}): </h1>
+                <h1>${total}</h1>
+              </div>
+              <div className="p-5">
+
+              <button className="primary-button w-full">Check Out</button>
+              </div>
+            </div>
+          </div>
+        </div>
+    </Layout>
+  );
+};
+
+export default cart;
--- next-tailwind-amazona/pages/cart.js
+++ next-tailwind-amazona/pages/cart.js
@@ -3,85 +3,70 @@ import React, { useContext } from "react";
 import { Layout } from "../components/Layout";
 import { Store } from "../utils/Store";
 import { TiDeleteOutline } from "react-icons/ti";
+import Link from "next/link";
 
-const cart = () => {
+const Cart = () => {
   const { state, dispatch } = useContext(Store);
-  const { cart } = state;
-  let total = 0
+  const {
+    cart: { cartItems },
+  } = state;
+  let total = 0;
 
-  const removeItemHandler = (slug) => {
+  return <Layout title="Shopping Cart">
+    <h1 className="mb-4 text-xl">Shopping Cart</h1>
+    {
+        cartItems.length === 0 ? <div>
+            Cart is empty. <Link href="/">Go Shopping</Link>
+        </div> : (
+            <div className="grid md:grid-cols-4 md:gap-5">
 
-    const existingCartItems = cart.cartItems.find((cartItem)=> cartItem.slug === slug)
-
-    if(existingCartItems.quantity===1){
-        return cart.cartItems.filter(cartItem => cartItem.slug !== slug)
-    }
-
-    return cart.cartItems.map((cartItem)=> cartItem.slug === slug ? {...cartItem,quantity:cartItem.quantity-1} : cartItem
-    )
-
-  }
-  
-
-  return (
-    <Layout title="Shopping Cart">
-      <div >
-        <h1 className=" text-lg font-[600]">Shopping Cart</h1>
-
-       
-          <div className=" grid grid-cols-1 md:grid-cols-4 justify-center gap-10">
-          <div className=" md:col-span-3">
-            <div className=" border-b border-b-gray-200" >
-              <ul className="grid grid-cols-4 justify-start items-center py-4 text-lg font-bold">
-                <li className="">Item</li>
-                <li>Quantity</li>
-                <li>Price</li>
-                <li>Action</li>
-              </ul>
-            </div>
-
-            <div>
-              {cart.cartItems.map((item, index) => {
-                total += item.price * item.quantity
-                return (
-                  <div key={index} className="grid grid-cols-4 justify-start items-center gap-2 border-b border-b-gray-200 py-4">
-                  <div className=" justify-start items-center gap-2 flex">
-                    <div className=" w-10 h-10">
-                      <Image
+                <div className=" overflow-x-auto md:col-span-3">
+                    <table className="min-w-full">
+                        <thead className="border-b">
+                            <tr>
+                                <th className="px-5 text-left">Item</th>
+                                <th className="p-5 text-right">Quantity</th>
+                                <th className="p-5 text-right">Price</th>
+                                <th className="p-5 ">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                       
+                            {
+                                cartItems.map((item)=> (
+                                    <tr key={item.slug} className="border-b">
+                                        <td>
+                                        <Link href={`/product/${item.slug}`}
+                                        className="flex items-center w-10 h-10 gap-1">
+                                        <Image
                         src={item.image}
                         alt={item.name}
-                        width={25}
-                        height={25}
+                        width={50}
+                        height={50}
                         layout="responsive"
                       />
-                    </div>
-                    <h1 className=" ">{item.name}</h1>
-                    </div>
-                    <h1>{item.quantity}</h1>
-                    <h1>${item.price}</h1>
-                    <h1>
-                      <TiDeleteOutline className=" cursor-pointer" size={30}
-                       onClick= {()=>removeItemHandler(item.slug)}/>
-                    </h1>
-                  </div>
-                );
-              })}
-            </div>
-</div>
-            <div className="card h-min">
-              <div className="mx-5 flex justify-between items-center mt-3 text-lg font-bold">
-                <h1 className=" ">Subtotal ({cart.cartItems.length}): </h1>
-                <h1>${total}</h1>
-              </div>
-              <div className="p-5">
+                      <h1>{item.name}</h1>
+                       
+                            </Link> 
+                                        </td>
 
-              <button className="primary-button w-full">Check Out</button>
-              </div>
+                                        <td className="p-5 text-right">
+                                            {item.quantity}
+                                        </td>
+                                        <td className="p-5 text-right">${item.price}</td>
+                                        <td className="p-5 text-center">
+                                            <TiDeleteOutline size={30} className=" cursor-pointer hover:fill-red-500"/>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                </div>
             </div>
-          </div>
-        </div>
-    </Layout>
-  );
+        )
+    }
+  </Layout>;
 };
 
-export default cart;
+export default Cart;
